cf0cfa0a075b83766df2f5f9ab712cb0d43b1f90
kwon woo <dirn0568@naver.com>
2023-01-27 13:27:11 +0900

    first commit

---
+++ src/main/java/com/example/cicdtest2/CicDtest2Application.java
@@ -0,0 +1,13 @@
+package com.example.cicdtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicDtest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicDtest2Application.class, args);
+    }
+
+}
