9974c97a2004bf54b4dacd674b6f233eac9c2c7d
kwon woo <dirn0568@naver.com>
2023-01-29 21:31:03 +0900

    141231214121234

--- src/main/java/com/example/cicdtest2/controller/chat/ChatRoomController.java
+++ src/main/java/com/example/cicdtest2/controller/chat/ChatRoomController.java
@@ -2,6 +2,7 @@ package com.example.cicdtest2.controller.chat;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller // @ResponseBody 필요할때 쓰기!
 @RequiredArgsConstructor
 public class ChatRoomController {
+    @ResponseBody
+    @GetMapping("/chatstest")
+    public void chatroomCreate2() {
+        System.out.println("로그야 찍혀라!@!!!!");
+    }
     @ResponseBody
     @PostMapping("/chatstest")
     public void chatroomCreate() {
