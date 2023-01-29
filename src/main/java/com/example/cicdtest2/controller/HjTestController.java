1312c715da4453edb856c3aac455a20ae2bd0d17
kwon woo <dirn0568@naver.com>
2023-01-29 17:56:17 +0900

    2131231231

---
+++ src/main/java/com/example/cicdtest2/controller/HjTestController.java
@@ -0,0 +1,20 @@
+package com.example.cicdtest2.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+public class HjTestController {
+
+    @GetMapping("/")
+    public String success() {
+
+
+        return "성공";
+    }
+
+}
