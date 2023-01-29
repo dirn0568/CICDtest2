858f5a40412311146532c3c7ddfb834af9b3ed7f
kwon woo <dirn0568@naver.com>
2023-01-29 17:52:24 +0900

    cicd chat test

---
+++ src/main/java/com/example/cicdtest2/controller/chat/ChatController.java
@@ -0,0 +1,27 @@
+package com.example.cicdtest2.controller.chat;
+
+import com.example.cicdtest2.dto.chat.ChatMessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+    private final SimpMessageSendingOperations messagingTemplate;
+    @ResponseBody
+    @MessageMapping("/chats") // socket 통신은 request를 안주나??? // 혹시 @Transactional 줘야하나???
+    public void message(ChatMessageDto chatMessageDto) {
+        System.out.println("33333333333333333333333333333333333333333333333");
+        System.out.println(chatMessageDto.getMessage());
+        System.out.println(chatMessageDto.getChatRoomId());
+        System.out.println(chatMessageDto.getUserEmail());
+        System.out.println("33333333333333333333333333333333333333333333333");
+
+        messagingTemplate.convertAndSend("/sub/chats/" + chatMessageDto.getChatRoomId(), chatMessageDto);
+    }
+}
