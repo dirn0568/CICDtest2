dcff48836c8268f7f13edfa90efac667f7a443fd
kwon woo <dirn0568@naver.com>
2023-01-31 15:50:42 +0900

    socket redis

--- src/main/java/com/example/cicdtest2/CicDtest2Application.java
+++ src/main/java/com/example/cicdtest2/CicDtest2Application.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class CicDtest2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(CicDtest2Application.class, args);
     }
-
 }
