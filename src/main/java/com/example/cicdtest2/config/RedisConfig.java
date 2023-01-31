b652cca0678dc1e335f62c9a15d8755251252b91
kwon woo <dirn0568@naver.com>
2023-01-31 17:12:12 +0900

    redis pub/sub hyunjun

--- src/main/java/com/example/cicdtest2/config/RedisConfig.java
+++ src/main/java/com/example/cicdtest2/config/RedisConfig.java
@@ -1,40 +1,55 @@
 package com.example.cicdtest2.config;
 
 
+import com.example.cicdtest2.dto.ChatMessage;
+import com.example.cicdtest2.service.RedisSubService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
-    @Value("${spring.redis.host}")
-    private String host;
-
-    @Value("${spring.redis.port}")
-    private int port;
+    //redisTemplate 설정
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessage.class));
         return redisTemplate;
     }
 
+    //리스너어댑터 설정
     @Bean
-    public StringRedisTemplate stringRedisTemplate() {
-        final StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
-        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
-        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
-        return stringRedisTemplate;
+    MessageListenerAdapter messageListenerAdapter() {
+        return new MessageListenerAdapter(new RedisSubService());
     }
+
+    //컨테이너 설정
+    @Bean
+    RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(messageListenerAdapter(), topic());
+        return container;
+    }
+
+    //pub/sub 토픽 설정
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic("topic1");
+    }
+
 }
---
+++ src/main/java/com/example/cicdtest2/service/RedisPubService.java
@@ -0,0 +1,16 @@
+package com.example.cicdtest2.service;
+
+import com.example.cicdtest2.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisPubService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void sendMessage(ChatMessage chatMessage) {
+        redisTemplate.convertAndSend("topic1", chatMessage);
+    }
+}
